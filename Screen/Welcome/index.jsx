36d4f6299cb8c7647ca8e651732949c778f302f5
carlop13 <carlosguadalupelt13@gmail.com>
2024-05-20 21:22:14 -0600

    add(web): add ToDo components
    se ha creado folder screem

---
+++ Screen/Welcome/index.jsx
@@ -0,0 +1,22 @@
+import React from "react";
+import "/DWI/intro-git/Screen/Welcome/welcome.css";
+
+function Index() {
+  
+    return (
+      <>
+        <div>
+            <h1>App con react</h1>
+            <br />
+            <p>Bienvenido a mi app donde podrás consulta temas interesantes</p>
+            <br />
+            <div className="estiloBoton">
+                <button className="boton1">Ir al listado de tareas</button>
+                <button>Más sobre mí</button>
+            </div>
+        </div>
+      </>
+    )
+  }
+
+  export default Index
