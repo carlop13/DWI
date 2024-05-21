3825529e101459340d555d7d8c7a7a7ea3908e22
carlop13 <carlosguadalupelt13@gmail.com>
2024-05-21 13:26:43 -0600

    Crear screen del listado de tareas

--- Screen/Welcome/ListadoTareas.jsx
+++ Screen/Welcome/ListadoTareas.jsx
@@ -1,8 +1,9 @@
 import React from 'react';
+import './TaskList.css';
 
 const ListadoTareas = () => {
   return (
-    <div>
+    <div className="tasklist-container">
       <h1>Listado de Tareas</h1>
       <p>Este es el contenido de la página "Listado de Tareas".</p>
     </div>
