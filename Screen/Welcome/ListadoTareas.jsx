aca3cb7a620b6d248d7ed7582776236896b1d076
carlop13 <carlosguadalupelt13@gmail.com>
2024-05-21 13:21:53 -0600

    Crear sistema de navegación

---
+++ Screen/Welcome/ListadoTareas.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const ListadoTareas = () => {
+  return (
+    <div>
+      <h1>Listado de Tareas</h1>
+      <p>Este es el contenido de la página "Listado de Tareas".</p>
+    </div>
+  );
+};
+
+export default ListadoTareas;
---
+++ Screen/Welcome/SobreMi.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const SobreMi = () => {
+  return (
+    <div>
+      <h1>Sobre Mí</h1>
+      <p>Este es el contenido de la página "Sobre Mí".</p>
+    </div>
+  );
+};
+
+export default SobreMi;
